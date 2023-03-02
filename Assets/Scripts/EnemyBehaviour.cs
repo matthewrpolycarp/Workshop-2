ac9db2ca20a47dc6c11a99ad74026e17b254092b
matthewrpolycarp <62485115+matthewrpolycarp@users.noreply.github.com>
2023-03-02 10:18:22 +0100

    m

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] patrolPoints;
+    public float chaseRange;
+    public GameObject projectile;
+
+    private int currentPatrolIndex;
+    private Transform player;
+    private bool isChasing;
+
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < chaseRange)
+        {
+            isChasing = true;
+        }
+        else
+        {
+            isChasing = false;
+        }
+
+        if (isChasing)
+        {
+            Chase();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+
+    void Chase()
+    {
+        transform.LookAt(player);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime);
+    }
+
+    void Patrol()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 1f)
+        {
+            currentPatrolIndex++;
+
+            if (currentPatrolIndex >= patrolPoints.Length)
+            {
+                currentPatrolIndex = 0;
+            }
+        }
     }
 }
